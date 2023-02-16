cc3c248f18e7d59fd6a0debdeb97b651afb0f292
Ricky C <kf6kjg@users.noreply.github.com>
2023-02-16 15:53:28 -0800

    feat: Allow for use as a filter
    Ignore files can now be passed though pipes for smudging or cleaning, allowing use as a gitattributes filter.

--- src/cli.ts
+++ src/cli.ts
@@ -1,16 +1,55 @@
+#!/usr/bin/env node
+
+import { createReadStream, createWriteStream } from "node:fs";
+import * as Path from "node:path";
+
+import * as FG from "fast-glob";
 import Minimist from "minimist";
 
 import { config } from "./config";
-import { ErrorHandling } from "./lib/transforms";
+import {
+	ErrorHandling,
+	IncludesFilterClean,
+	IncludesFilterSmudge,
+	IncludesFilterSmudgeOptions,
+} from "./lib/transforms";
 
-import { TransformFilesOptions } from ".";
+//* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+export interface TransformFilesOptions extends IncludesFilterSmudgeOptions {
+	/** Base path for the patterns defined in the files. */
+	cwd: string;
+	/** The files that should be converted. File and path globs are supported. */
+	files: string[];
+}
 
 const argv = Minimist(process.argv.splice(2));
-if (argv._.length <= 0) {
+
+const opts: TransformFilesOptions = {
+	cwd: process.cwd(),
+	files: (argv._ as string[]).filter((value) => value !== "$0"),
+	errorHandling:
+		argv.errorHandling === ErrorHandling.throwImmediate
+			? ErrorHandling.throwImmediate
+			: ErrorHandling.embedAsComments,
+};
+
+/*
+process.stdin.isTTY is true when the process is being run in an environment with a TTY and there's no data being piped in.
+
+process.stdin.isTTY is falsy when:
+1. The stdin is being routed from a pipe.
+2. The process is being run in an environment without a TTY.
+
+Thus process.stdin.isTTY is not a good indicator of whether or not there's input on stdin. It only can be use to prove that there's a TTY or not.
+*/
+
+if (opts.files.length <= 0) {
 	process.stderr.write(`Missing file patterns!
-[giismudge|giiclean] [errorHandling=embedAsComments|throwImmediate] filePattern...
-If invoked as giismudge it will replace the included ignore directives in each file that matches each pattern.  Useful for updating your files!
+[giismudge|giiclean] [errorHandling=embedAsComments|throwImmediate] [filePattern...]
+If invoked as giismudge it will replace the included ignore directives in each file that matches each pattern. Useful for updating your files!
 If invoked as giiclean it will strip the included ignore directives out of each file that matches each pattern.
+
+If you are looking to process text through a pipe, simply pipe the data in and pass "-" as the file pattern. The output will be put on stdout.
 
 Options
     errorHandling: Either 'embedAsComments', the default, to cause errors
@@ -20,15 +59,80 @@ Options
 	process.exit(1);
 }
 
-export const opts: TransformFilesOptions = {
-	cwd: process.cwd(),
-	files: (argv._ as string[]).filter((value) => value !== "$0"),
-	errorHandling:
-		argv.errorHandling === ErrorHandling.throwImmediate
-			? ErrorHandling.throwImmediate
-			: ErrorHandling.embedAsComments,
-};
+if (opts.files.includes("-")) {
+	// Pipe filter mode.
+	const transformer =
+		Path.basename(process.argv[1]) === "giismudge"
+			? new IncludesFilterSmudge(opts)
+			: new IncludesFilterClean(opts);
 
-process.stderr.write(
-	`gitignore-include ${config.npmConfig?.version ?? "version unknown!?"}\n`
-);
+	transformer.pipe(process.stdout);
+
+	try {
+		process.stdin.pipe(transformer);
+	} finally {
+		process.stdin.resume();
+	}
+} else {
+	// File processor mode.
+	process.stderr.write(
+		`gitignore-include ${config.npmConfig?.version ?? "version unknown!?"}\n`
+	);
+
+	(async (): Promise<void> => {
+		for await (let file of FG.stream(opts.files, {
+			cwd: opts.cwd,
+			dot: true,
+		})) {
+			if (file instanceof Buffer) {
+				file = file.toString("utf8");
+			}
+
+			const filePath = Path.resolve(opts.cwd, file);
+
+			const fileSource = createReadStream(filePath, { encoding: "utf8" });
+
+			const filterResults = await new Promise<string[]>((resolve, reject) => {
+				const result: string[] = [];
+
+				const filter =
+					Path.basename(process.argv[1]) === "giismudge"
+						? new IncludesFilterSmudge(opts)
+						: new IncludesFilterClean(opts);
+
+				filter.on("data", (data) => {
+					result.push(data.toString());
+				});
+
+				filter.on("end", () => {
+					resolve(result);
+				});
+
+				filter.on("error", (error) => {
+					reject(error);
+				});
+
+				fileSource.on("end", () => {
+					filter.end();
+				});
+
+				fileSource.pipe(filter);
+			});
+
+			if (filterResults.length) {
+				const fileSink = createWriteStream(filePath, { encoding: "utf8" });
+				fileSink.write(filterResults.join(""));
+				fileSink.end();
+			}
+		}
+	})().catch((reason) => {
+		if (reason instanceof Error) {
+			process.stderr.write(reason.stack ?? reason.message);
+		} else {
+			process.stderr.write(
+				typeof reason === "string" ? reason : JSON.stringify(reason)
+			);
+		}
+		process.exit(1);
+	});
+}
